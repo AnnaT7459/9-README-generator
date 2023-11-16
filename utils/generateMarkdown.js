489884b2e93ea225e5e033cbfa4ea9d46adad9e4
AnnaT7459 <141446349+AnnaT7459@users.noreply.github.com>
2023-11-15 21:16:32 -0600

    used solution code from mini challenge to reference how to include packages needed for the app AND reset prior commit to add a gitignore file for node_modules

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -5,7 +5,7 @@ function renderLicenseBadge(license) {}
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {}
-
+https://gist.github.com/lukas-h/2a5d00690736b4c3a7ba
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
