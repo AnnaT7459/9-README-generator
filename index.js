225c2d2b1c64153f62bbb14c0922b889a295ad65
AnnaT7459 <141446349+AnnaT7459@users.noreply.github.com>
2023-11-15 21:32:05 -0600

    added .DS_Store/ to gitignore file and started adding question prompts for user input (used solution code from mini project for reference)

--- index.js
+++ index.js
@@ -2,7 +2,42 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your application?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: '',
+    //   How do I add more questions to the description section?
+    // 'How do you use your application?
+    },
+    {
+      type: 'input',
+      name: 'hobby',
+      message: 'What is your favorite hobby?',
+    },
+    {
+      type: 'input',
+      name: 'food',
+      message: 'What is your favorite food?',
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub Username',
+    },
+    {
+      type: 'input',
+      name: 'linkedin',
+      message: 'Enter your LinkedIn URL.',
+    },
+  ];
+
+  inquirer.createPromptModule(questions)
 
 
 // TODO: Create a function to write README file
